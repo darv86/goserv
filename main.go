d0db4bb53b616ff0b5fcf14f9430d063b366fe32
Dima R <86darv@gmail.com>
2024-09-26 16:36:40 +0300

    method for struct

--- main.go
+++ main.go
@@ -5,57 +5,27 @@ import "fmt"
 func main() {
 	var print = fmt.Printf
 
-	// Instance creation
 	myCar := Car{
 		nameCar: "mazda",
 		wheels:  4,
-		// embedded struct can be created without having struct previously
-		Engine: Engine{nameEngine: "rolls"},
+		engine:  struct{ nameEngine string }{nameEngine: "rolls"},
 	}
 
-	// Instance creation alternative (separate fields assignment)
-	// myCar := Car{}
-	// myEngine := Engine{}
-	// myEngine.nameEngine = "royal"
-
-	// myCar.Engine = myEngine
-	// myCar.nameCar = "mazda"
-	// myCar.wheels = 4
-
-	// Embedded struct
-	// now can assign a value straight to a key of the Engine struct
-	// myCar.nameEngine = "rolls royce"
-
-	// Struct initialization
-	// regular assignment typed value to the key
-	// myCar.engine = myEngine
-	// also can be done with anonymous struct
-	// myCar.engine = struct{ name string }{name: "jaguar"}
-
-	print("there is res1 - %v", myCar)
+	print("there is result - %v, %s", myCar, myCar.getInfo(";"))
 }
 
-// Embedded struct
 type Car struct {
 	nameCar string
 	wheels  int
-	// struct Car will have all fields of Engine struct
-	Engine
+	engine  struct{ nameEngine string }
 }
 
-type Engine struct {
-	nameEngine string
+// Method for struct Cat
+func (c Car) getInfo(separator string) string {
+	return fmt.Sprintf(
+		"name: %s%s engine: %s",
+		c.nameCar,
+		separator,
+		c.engine.nameEngine,
+	)
 }
-
-// Struct initialization
-// type Car struct {
-// 	name   string
-// 	wheels int
-// 	// regular field typing
-// 	// engine Engine
-// 	// also can be done with anonymous struct as well (in this case contracts needs to be valid)
-// 	engine struct{ name string }
-// }
-// type Engine struct {
-// 	name string
-// }
