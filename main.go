0d36a2ee02e649338c706d44012fcdc36f058288
Dima R <86darv@gmail.com>
2024-11-01 21:00:19 +0200

    interfaces; type parameters

--- main.go
+++ main.go
@@ -2,98 +2,149 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
 func main() {
-	list := map[string]int{"bob": 1, "john": 2, "ann": 3}
-	// Mutex initialization
-	mtx := &sync.Mutex{}
-	go adder(list, mtx)
-	go multiplier(list, mtx)
-	time.Sleep(time.Second)
-	fmt.Println("final list after Mutex:", list)
+	arrInt := []int{1, 2, 3, 4}
+	slc1, slc2 := splitSlice(arrInt)
+	fmt.Println("generics", slc1, slc2)
 
-	rMtx := &sync.RWMutex{}
-	data := &[]int{}
-	go reader1(data, rMtx)
-	go reader2(data, rMtx)
-	go writer(data, rMtx)
-	// infinite loop
-	// select {}
+	arrStr := []string{"one", "two", "three"}
+	fmt.Println("last element:", getLast(arrStr))
+	fmt.Println("last zero element:", getLast([]int{}))
 
-	a := []int{1}
-	func(data []int) {
-		// slice changes are visible outside this func
-		// data[0] = 2
-		// slice changes are not visible outside this func
-		// and append creates copy of a slice (data)
-		data = append(data, 2)
-		fmt.Println("final data:", data)
-	}(a)
-	fmt.Println("final data:", a)
+	user := &user{name: "bob"}
+	admin := &admin{name: "ann"}
+	fmt.Println("converter result (user):", converter(user))
+	fmt.Println("converter result (admin):", converter(admin))
+
+	fmt.Println("first element:", getFirst(arrStr))
+	// Custom type (myInt) creation with underlying type of int
+	type myInt int
+	myArrInt := []myInt{1, 2, 3, 4}
+	// error if argument for gerFirst is custom type (myInt)
+	// or there is no ~ syntax inside interface (listItem)
+	fmt.Println("first element:", getFirst(myArrInt))
+
+	// vault implements store interface,
+	// which has type parameter (si),
+	// this allows to create two vaults (string, int),
+	// then use the same methods (getVault, addToVault),
+	// and func (vaultLogger) on these vaults
+	vaultInt := &vault[int]{items: []int{}}
+	vaultInt.addToVault(1, 2, 3)
+	vaultLogger(vaultInt)
+	vaultStr := &vault[string]{items: []string{}}
+	vaultStr.addToVault("one", "two", "three")
+	vaultLogger(vaultStr)
+	fmt.Println("data from both vaults: ", vaultStr.getVault(), vaultInt.getVault())
 }
 
-func writer(list *[]int, rMtx *sync.RWMutex) {
-	// RLock method blocks data (list) only for writing
-	// all functions (reader1, reader2, writer),
-	// which have mutex (rMtx) with RLock method
-	// can read data (list) at the same time,
-	// but write at the same time can only one
-	rMtx.RLock()
-	defer rMtx.RUnlock()
-	for i := 0; ; i++ {
-		// If the underlying array’s capacity is exceeded,
-		// (e.g.: append exceeds underlying array’s capacity)
-		// append creates a new array,
-		// and the slice within the function
-		// won’t reflect this change outside the function
-		// that's why pointer is used here:
-		// this func changes slice (list),
-		// using reference on the original array
-		*list = append(*list, i)
-		fmt.Println("from writer:", (*list)[i])
-		time.Sleep(500 * time.Millisecond)
-	}
-
+type si interface {
+	string | int
 }
 
-func reader1(list *[]int, rMtx *sync.RWMutex) {
-	rMtx.RLock()
-	defer rMtx.RUnlock()
-	for {
-		fmt.Println("from reader1:", *list)
-		time.Sleep(500 * time.Millisecond)
-	}
+// Type parameter in interface
+type store[P si] interface {
+	// type parameter can be passed
+	// to the specific method of an instance,
+	// which implements this interface
+	getVault() []P
+	addToVault(...P)
 }
 
-func reader2(list *[]int, rMtx *sync.RWMutex) {
-	rMtx.RLock()
-	defer rMtx.RUnlock()
-	for {
-		fmt.Println("from reader2:", *list)
-		time.Sleep(500 * time.Millisecond)
-	}
+// store interface is needed to have one func,
+// which can accept two types (string, int = si) of vaults;
+// methods in go can't have generic type and
+// custom interface with constraints as a type for parameters:
+// func (mo *mObj) addToVault(item unit)
+// but functions can:
+func vaultLogger[P si](vault store[P]) {
+	fmt.Printf("data from %T: %v\n", vault, vault.getVault())
 }
 
-func adder(list map[string]int, mtx *sync.Mutex) {
-	// Lock method blocks data (list) from reading and writing,
-	// while this function (adder) executes
-	mtx.Lock()
-	// Unlock method unblock data, after this func is done
-	defer mtx.Unlock()
-	for name, id := range list {
-		list[name] = id + 2
-		fmt.Println("from adder name:", name, "--- id:", list[name])
-	}
+// Type parameter in struct;
+// type parameter (P si) further
+// can be used for the specific method
+type vault[P si] struct {
+	items []P
 }
 
-func multiplier(list map[string]int, mtx *sync.Mutex) {
-	mtx.Lock()
-	defer mtx.Unlock()
-	for name, id := range list {
-		list[name] = id * 2
-		fmt.Println("from multiplier name:", name, "--- id:", list[name])
+// value receiver method works fine with pointer (vaultInt),
+// because Go automatically dereferences the pointer;
+// Type parameters in the method of the vault instance
+func (v vault[P]) getVault() []P {
+	return v.items
+}
+
+// use pointer receiver method,
+// if you need to modify the struct (vaultInt);
+// Type parameters in the method of the vault instance
+func (v *vault[P]) addToVault(item ...P) {
+	v.items = append(v.items, item...)
+}
+
+// -----------------------------
+
+func getFirst[T listItem](arr []T) T {
+	element := arr[0]
+	return element
+}
+
+type listItem interface {
+	// Union type constraint
+	// generic (listItem) func (getFirst)
+	// can accept only array of strings or ints
+	// string | int
+	// ~ syntax says that any type,
+	// that has underlying type of string,
+	// can be type of listItem
+	// if there is no ~, strict type of string or int should be in use
+	~string | ~int
+}
+
+// this generic (T) has constraint (stringer) via interface:
+// incoming argument (src) can be any struct,
+// that implements interface (stringer)
+func converter[T stringer](src T) string {
+	return fmt.Sprint("-> ", src.getName())
+}
+
+type stringer interface {
+	getName() string
+}
+
+type user struct {
+	name string
+}
+type admin struct {
+	name string
+}
+
+func (u *user) getName() string {
+	return u.name
+}
+func (a *admin) getName() string {
+	return a.name
+}
+
+// -------------------------------
+
+func getLast[T any](store []T) T {
+	if len(store) == 0 {
+		// to return zero value of the specific type
+		// use this syntax
+		var zeroVal T
+		return zeroVal
 	}
+	item := store[len(store)-1]
+	return item
+}
+
+// -------------------------------
+
+func splitSlice[T any](slice []T) ([]T, []T) {
+	mid := len(slice) / 2
+	fmt.Println("mid", mid)
+	return slice[:mid], slice[mid:]
 }
