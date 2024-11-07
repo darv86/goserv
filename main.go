d947d77aae53068b6531f7355482b8257e51d55d
Dima R <86darv@gmail.com>
2024-11-07 18:16:08 +0200

    func renderTemplate added; page.html template added; db/index.txt, db/login.txt added

--- main.go
+++ main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"text/template"
 )
 
-func mainRouter(w http.ResponseWriter, r *http.Request) {
-	log.Println("from router main:", r.Host)
-	fmt.Fprint(w, "from router main:", r.Host)
+type Page struct {
+	Title string
+	Body  string
 }
 
-func apiRouter(w http.ResponseWriter, r *http.Request) {
-	log.Println("from router api: ", r.Host)
-	fmt.Fprint(w, "from router api: ", r.Host)
+func loadPage(name string) (*Page, error) {
+	filePath := "db/" + name + ".txt"
+	body, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: name, Body: string(body)}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, name string, page *Page) {
+	t, _ := template.ParseFiles("static/" + name + ".html")
+	t.Execute(w, page)
+}
+
+func indexRouter(w http.ResponseWriter, r *http.Request) {
+	path := strings.Split(r.URL.Path, "/")[1]
+	if len(path) == 0 {
+		path = "index"
+	}
+	log.Println("path from index:", path)
+	page, err := loadPage(path)
+	if err != nil {
+		page = &Page{Title: "Wrong page title content", Body: "Wrong page body content"}
+	}
+	renderTemplate(w, "page", page)
+	log.Println("router index:", r.Host, r.URL.Path)
+}
+
+func loginRouter(w http.ResponseWriter, r *http.Request) {
+	path := strings.Split(r.URL.Path, "/")[1]
+	if len(path) == 0 {
+		path = "index"
+	}
+	log.Println("path from login:", path)
+	page, err := loadPage(path)
+	if err != nil {
+		page = &Page{Title: "Wrong page title content", Body: "Wrong page body content"}
+	}
+	renderTemplate(w, "page", page)
+	log.Println("router login:", r.Host, r.URL.Path)
 }
 
 func main() {
 	PORT := "8080"
-	http.HandleFunc("/", mainRouter)
-	http.HandleFunc("/api", apiRouter)
+	http.HandleFunc("/", indexRouter)
+	http.HandleFunc("/login", loginRouter)
 	http.ListenAndServe(":"+PORT, nil)
 	log.Printf("port: %s", PORT)
 }
